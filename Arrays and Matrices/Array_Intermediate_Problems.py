f2fd5cc328f94ae74d48ed21e205af8483ad8de0
Dijoraj Senroy <36783331+dijorajsenroy@users.noreply.github.com>
2021-01-20 01:54:42 +0530

    added Kadane's algorithm

--- Arrays and Matrices/Array_Intermediate_Problems.py
+++ Arrays and Matrices/Array_Intermediate_Problems.py
@@ -39,9 +39,8 @@ overflow. Thus the water contained at ith bar = min(lmax, rmax) - arr[i](height 
 water, this solution takes O(n*n) time. We can optimise this solution and make it O(n) time by making two arrays to store
 lmax and rmax of all elements, thus making space complexity O(n).
 
-"""
 trapwater = lambda arr: sum([min(max(arr[i:]), max(arr[:i+1])) - arr[i] for i in range(1, len(arr)-1)])
-
+"""
 def rainwater(arr):
     water = 0
     for i in range(1, len(arr)-1):
@@ -72,7 +71,26 @@ def rainwaterOn(arr):
     return res
         
 """
-3. Majority Element Problem:
+3. Equillibrium Index of Array:
+
+The equillibrium index of an array is the index such that the sum of elements preceding it equals the sum of
+elements following it. The naive solution would be to compute the subarray sums of all elements before and after
+for each element. This would take O(n*n) time complexity. The better O(n) solution would be to first obtain the 
+total sum of the array, then traverse the array and add elements to the left sum. The right sum would be the 
+total - current element - left sum. When these values are equal we have our element index.
+"""
+def equillibrium(arr):
+    total = sum(arr)
+    leftsum = 0
+    for i in range(len(arr)):
+        rightsum = total - arr[i] - leftsum
+        if leftsum == rightsum:
+            return i
+        else:
+            leftsum += arr[i]
+
+"""
+4. Majority Element Problem:
 
 An element in an array is called a majority if it appears more than n/2 times. The simple pythonic O(n*n) solution
 is to use list count in a loop traversing the arrays. The efficient solution takes O(1) auxillary space and
@@ -90,7 +108,8 @@ def majority(arr):
             return i
     return -1
 
-def murrayvoting(arr):
+
+def MurraysVotingAlgorithm(arr):
     # 1st phase gives a candidate for majority element
     res = 0
     count = 1
@@ -110,20 +129,55 @@ def murrayvoting(arr):
         return -1
 
 """
-4. Equillibrium Index of Array:
+6. Sliding Window Technique:
 """
 """
-5. Sliding Window Technique:
+7. Prefix Sum Technique:
 """
 """
-6. Prefix Sum Technique:
+8. Maximum Sum Sub-Array:
+
+Given an array we have to compute the maximum sum that a subarray of the array posesses, where a subarray is a subset of
+contiguous elements present in the array. When there are negative elements the max sum is not the sum of all elements. Thus 
+We have to generate all subarrays and check their sums. The naive solution is to use two loops to make all subarrays and store
+the sum and maximum sum in two variables. for an ith element we run a loop i+1 to n, which gives all subset sums in O(n*n) time.
+
+The efficient method to do this is using Kadane's algorithm to compute maximum subarray sum, in O(n) time complexity. Steps:
+(i) We traverse the elements from left to right and for ith element we compute the max subarray sum, for subarrays ending with i.
+(ii) To compute the max sum of i elements we add ith element to the max sum of i-1 elements and thus re-use the previous sum 
+(iii) After computing the maximum ending sum of all i elements we find the max of these values and that gives the result.
+
+Thus the recursive statement for this problem will be formed as:
+Sum(i) = max(Sum(i-1) + arr[i], arr[i])
+This works by starting a new subarray when the previous sum is smaller, or else adding to the same subarray. For example in 
+the given array [-5, 2, -2, 4], the maxEnding sum is starting with -5 at 0th index. at 1st index since 2 > -5+2 its 2, at third
+index its 0, and at 4th its 4. The idea of this algorithm is that the greatest maxEnding sums wuld produce the max sum. 
+"""
+import sys
+def maxSumNaive(arr):
+    res = - sys.maxsize
+    for i in range(0, len(arr)):
+        currsum = arr[i]
+        for j in range(i+1,len(arr)):
+            currsum += arr[j]
+            res = max(res, currsum) 
+    return res
+
+
+def KadanesAlgorithm(arr):
+    # max ending sum initialised with first element
+    maxEndingSum = arr[0]
+    maxSubarraySum = 0
+    for i in range(1, len(arr)):
+        # continuing subarray sum or starting a new one
+        maxEndingSum = max(maxEndingSum + arr[i], arr[i])
+        # saving max subarray sum
+        maxSubarraySum = max(maxSubarraySum, maxEndingSum)
+    return maxSubarraySum
+
+"""
+9. Longest Even Odd Sub-Array:
 """
 """
-7. Maximum Sum Sub-Array:
-"""
-"""
-8. Longest Even Odd Sub-Array:
-"""
-"""
-9. Maximum Circular Sum Sub-Array:
+10. Maximum Circular Sum Sub-Array:
 """
