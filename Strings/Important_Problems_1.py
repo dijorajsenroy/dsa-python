12b3fcd04b0720163c497cf01369bfe1e841b707
Dijoraj Senroy <36783331+dijorajsenroy@users.noreply.github.com>
2021-02-01 18:57:43 +0530

    added Anagram Search problem

--- Strings/Important_Problems_1.py
+++ Strings/Important_Problems_1.py
@@ -95,10 +95,73 @@ def reverseWords(s):
 
 """
 5. Check if Strings are rotations:
+
+Check if a string s2 can be obtained from s1 by rotating it any number of times. The type of rotation can be left or right rotation.
+The Naive solution is O(n*n) for searching and slicing in the loop. We can compute the concatenation of s1+s1 and thus we have
+all the rotations in it. Searching s2 in s1+s2 would take O(n) time complexity.
 """
+def checkRotNaive(s1, s2):
+    leftrot = s1; rightrot = s1
+    for _ in range(len(s1)):
+        leftrot = leftrot[1:] + leftrot[0]
+        rightrot = rightrot[-1] + rightrot[:-1]
+        if leftrot == s2 or rightrot == s2:
+            return True
+    return False
+
+checkRot = lambda s1, s2: s2 in s1*2
+
 """
-6. Anagram Search:
+6. Anagram Search or Matching all permutations of a pattern in text:
+
+Check if any permutation of a pattern is present in the string. The Naive solution is to slide the pattern over the txt and check 
+if the pair is an anagram. We can check for anagram in O(len(pat)) time, thus the time complexity of this solution is the same as
+the naive pattern matching algorithm, ie, O((n-m+1)*m). The efficient solution here again is to use the character - indexed count 
+array approach, but since dictionaries are not useful here we will implement character array using ord(char) as index. For this
+solution we are essentially modifying the Rabin Karp Algorithm by matching the counts of the pattern and string. Steps:
+
+(i) For every ith character in pat, we increment the value stored in it's corresponding index in the count array of txt and pat.
+The count_txt array stores the value of current window of text and count_pat stores the frequency of pattern elements. 
+(ii) In a loop from len(pat) to len(txt) - 1, we do the following:
+    (a) If the count arrays are equal we found an occurrence of the pattern. Print index.
+    (b) Else we virtually slide the count_txt window by adding the next frequency and subtracting the first. Hence we are sliding
+    the frequency window of the letters in that substring which will be compared with the frequencies of the pattern.
+    (c) We increment the count of the current character in count_txt and decrement the count of prev first character.
+(iii) Check if the count arrays are equal for the last window in the array as well.
 """
+def anagramSearchNaive(txt, pat):
+    for i in range(len(txt) - len(pat) + 1): # O(n-m+1)
+        if anagram(txt[i:i+len(pat)], pat):
+            return True
+    return False
+
+def anagramSearch(txt, pat):
+
+    # making two count arrays (constant space)
+    count_txt = [0]*256# substring window frequencies
+    count_pat = [0]*256 # pattern frequencies
+
+    # Initialising window and pattern frequencies for 1st window
+    for i in range(len(pat)):
+        count_txt[ord(txt[i])] += 1
+        count_pat[ord(pat[i])] += 1
+    
+    # Sliding the window 
+    for i in range(len(pat), len(txt)):
+        # if the count arrays are same we have a match
+        if count_txt == count_pat:
+            print("Found at, ", i - len(pat))
+
+        # increasing frequency of current character 
+        count_txt[ord(txt[i])] += 1
+        # decreasing frequency of first character of previous window
+        count_txt[ord(txt[i - len(pat)])] -= 1
+        
+    # Checking for last window
+    if count_txt == count_pat:
+        print("Found at, ", len(txt)-len(pat))
+    
+
 """
 7. Lexicographic Rank of a string:
 """
