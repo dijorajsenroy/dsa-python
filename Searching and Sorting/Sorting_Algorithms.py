3a7c8b916fe69df1a4ed9aa1328a78e977f04b45
Dijoraj Senroy <36783331+dijorajsenroy@users.noreply.github.com>
2021-02-05 21:20:56 +0530

    Update Sorting_Algorithms.py

--- Searching and Sorting/Sorting_Algorithms.py
+++ Searching and Sorting/Sorting_Algorithms.py
@@ -71,16 +71,19 @@ two the j-1th element is more than the jth element. This ensures the j=ith value
 
 """
 
-def insertionSort(arr):
-	for i in range(1, len(arr)):
-		# element to be inserted in arr[0]...arr[i-1] subarray
-		key = arr[i]
-		j = i - 1
-		while j >= 0 and key < arr[j]:
-			# swap arr[j+1](initially key) and arr[j]
-			# till we find a value greater than key and stop 
-			arr[j+1], arr[j] = arr[j], arr[j+1]
-			j -= 1
+def insertionSort(arr): 
+	# Traverse through 1 to len(arr) 
+	for i in range(1, len(arr)): 
+		key = arr[i] 
+  		# Move elements of arr[0..i-1], that are 
+        	# greater than key, to one position ahead 
+        	# of their current position 
+        	j = i-1
+        	while j >=0 and key < arr[j] : 
+                	arr[j+1] = arr[j] 
+                	j -= 1
+        	arr[j+1] = key
+	
 	return arr
 
 """
