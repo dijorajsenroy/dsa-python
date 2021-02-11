079a004a7b3a20771f0b321dc59f28ad434143a4
Dijoraj Senroy <36783331+dijorajsenroy@users.noreply.github.com>
2021-02-12 00:00:25 +0530

    Added Problem List

--- Searching and Sorting/Searching_Problems.py
+++ Searching and Sorting/Searching_Problems.py
@@ -1,2 +1,33 @@
 """
+1. A. Binary Search (Iterative Implementation):
+1. B. Binary Search (Recursive Implementation):
+"""
+"""
+2. A. Index of first occurrence in a sorted array:
+2. B. Index of last occurence in a sorted array:
+"""
+"""
+3. Count occurrences in a sorted array:
+"""
+"""
+4. Count 1s in a sorted binary array:
+"""
+"""
+5. A. Search in an Infinite Sized Array:
+5. B. Search in a Sorted Rotated Array:
+"""
+"""
+6. Square Root Problem:
+"""
+"""
+7. Find peak elements:
+"""
+"""
+8. Repeating Elements:
+"""
+"""
+9. Allocate minimum pages:
+"""
+"""
+10. Two pointer approach:
 """
