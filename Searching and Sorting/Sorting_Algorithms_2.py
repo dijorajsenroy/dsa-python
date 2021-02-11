e1b5f2efdfa8eca189810951b9df15118f75f789
Dijoraj Senroy <36783331+dijorajsenroy@users.noreply.github.com>
2021-02-11 23:22:28 +0530

    Counting Sort added

---
+++ Searching and Sorting/Sorting_Algorithms_2.py
@@ -0,0 +1,18 @@
+"""
+1. Cycle Sort:
+"""
+"""
+2. Heap Sort:
+"""
+"""
+3. Counting Sort:
+
+Counting sort algorithm is a linear time algorithm 
+"""
+"""
+4. Radix Sort:
+"""
+"""
+5. Bucket Sort:
+"""
+
