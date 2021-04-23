fef1561748550c85033adf57c648cee0fbc58bc1
Dijoraj Senroy <36783331+dijorajsenroy@users.noreply.github.com>
2021-04-23 13:44:42 +0530

    Added Merge Sort for Linked Lists

--- Linked Lists/Linked_List_Problems.py
+++ Linked Lists/Linked_List_Problems.py
@@ -41,12 +41,12 @@ def searchList(current, key):
     else:
         return searchList(current.next, key)
 
-def reverseTraversal(last):
+def reverseTraversalDLL(last):
     # last should refer to tail node
     if last is None:
         return 
     print(last.data)
-    return reverseTraversal(last.prev)
+    return reverseTraversalDLL(last.prev)
 
 """
 3. Detect and remove loops in a Linked List:
@@ -122,19 +122,103 @@ def detectAndRemove(head):
         fast.next = None
     else:
         print("No Cycle Detected")
-        
+
 """
-4. Find Intersection point of two Linked Lists:
+4. Merge Sort for Linked Lists:
+
+I. Function to obtain middle node: 
+We use a variation of the tortoise and hare algorithm to obtain mid. fast pointer traverses the list two paces 
+at a time till it reaches the last or second last node. Mean while for every two paces of the fast pointer,
+the slow pointer traverses one pace. Hence at terminating condition, when fast pointer reaches the end of the 
+list the slow pointer reaches the middle position which is then returned. This takes O(n) time and O(1) space. 
+"""
+def getMid(head):
+    if head is None:
+        return head
+    # use tortoise and hare method to find mid
+    slow = head; fast = head
+    while(fast.next.next != None and fast.next != None):
+        slow = slow.next
+        fast = fast.next.next
+    return slow
+    
+"""
+II. Function to merge sorted halves: 
+The steps involved in algorithm to merge two sorted halves into one list is,
+(i) Initialize result list as empty: head = NULL. Let left and right be the heads of first and second lists. 
+(ii) Create a function to reverse a linked list, reverse left and right lists. 
+(iii) While (a != NULL and b != NULL)
+    a) Use pointers to traverse the 2 lists. Find the larger of the two pointers.
+    b) Insert the larger value of node at the front of result list.
+    c) Move ahead in the list of larger node. 
+(iv) If right becomes NULL before left, insert all nodes of left into result list at the beginning.
+(v)) If left becomes NULL before right, insert all nodes of right into result list at the beginning. 
+"""
+def mergeSortedHalves(left, right):
+    # reverse the lists left and right
+    left = reverse(left)
+    right = reverse(right)
+    head = None
+
+    # inserting the larger nodes of left and right into result
+    while left != None and right != None:
+        # compare the two pointers
+        if left.data >= right.data:
+            temp = left.next # store the next node
+            left.next = head # add larger node to result list
+            left = temp # move ahead in larger list
+        else:
+            temp = right.next  # store the next node
+            right.next = head  # add larger node to result list
+            right = temp  # move ahead in larger list
+
+    # if right is not null - insert remaining nodes 
+    while right is not None:
+        temp = right.next # storing next node temporarily
+        right.next = head # inserting next node in result list
+        head = right # making inserted node the new head
+        right = temp # traverse right to next node
+
+    # if left is not null - insert remaining nodes
+    while left is not None:
+        temp = left.next  # storing next node temporarily
+        left.next = head  # inserting next node in result list
+        head = left  # making inserted node the new head
+        left = temp  # traverse left to next node
+    return head
+"""        
+III. Merge Sort Function:
+The merge sort algorithm for linked lists is described below in the following steps:
+(i) If head is null, return because there is only one element in the linked list.
+(ii) Make a function to obtain mid for linked lists. Use it to obtain mid and divide the linked list in 2 halves.
+(iii) Recursively apply the merge sort function on the two halves. 
+(iv) Make a function to sort merged halves and update the head pointer to first node.
+"""
+def mergeSort(head):
+    # check if list is empty or has one node
+    if head is None or head.next is None:
+        return head
+    else:
+        # use function to obtain mid
+        mid = getMid(head)
+        # we break the list at mid
+        secondhalf = mid.next
+        mid.next = None
+        # recursively sort the two halves
+        left = mergeSort(head) # left half: head to mid
+        right = mergeSort(secondhalf) # right half: mid.next to tail
+        # use function to merge sorted halves
+        return mergeSortedHalves(left, right)
+
+"""
+5. LRU Cache Design Problem:
 """
 """
-5. Removes Duplicates from a sorted Linked List:
+6. Find Intersection point of two Linked Lists:
 """
 """
-6. Merge Sort for Linked Lists:
+7. Removes Duplicates from a sorted Linked List:
 """
 """
-7. Palindrome Linked Lists:
-"""
-"""
-8. LRU Cache Design Problem:
+8. Palindrome Linked Lists:
 """
